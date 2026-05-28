5e9ecf0bd95e58d796438d3ecd8c7afd7e76925b
hasheninfo <hansakagaa@gmail.com>
2026-05-28 07:59:39 +0530

    init preloader

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,3 +3,20 @@
  * Copyright @ 2026 The Ashen Hansaka Authors
  * Licensed under MIT
  */
+
+(function() {
+  "use strict";
+
+
+
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+})();
