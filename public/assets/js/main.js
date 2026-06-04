641bc1482ac63b7970219eab80dee6fe86cf43a0
hasheninfo <hansakagaa@gmail.com>
2026-06-05 00:35:29 +0530

    init animation on scroll function

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -179,6 +179,19 @@
   });
 
   /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
    * Preloader
    */
   const preloader = document.querySelector('#preloader');
